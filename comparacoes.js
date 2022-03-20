bb03d5f78158e24a9aa51065ae1e18b6cb77317d
Yumiko <dev.atae02@gmail.com>
2022-03-20 10:59:56 -0300

    Anotações aula 4 - comparações, templates, ternario

---
+++ comparacoes.js
@@ -0,0 +1,20 @@
+// == (comparação implícita) só compara valor
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto)
+
+// typeof
+
+console.log(typeof texto)
+console.log(typeof numero)
+
+// === compara o valor e o tipo de dado
+console.log(numero === texto)
+
+// Operadores
+
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação, da mesma forma que == e === retornam true ou false.
---
+++ templates-strings.js
@@ -0,0 +1,9 @@
+const nome = "Jacque";
+const idade = 2022 - 2002;
+const cidadeDeNascimento = "São Paulo";
+
+//const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao);
